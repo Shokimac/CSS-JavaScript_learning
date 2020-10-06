bf8cf2ab8b1c6ee8b969b9d0bd4cc1d8722e5e1f
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-06 19:20:47 +0900

    プロパティとメソッドについて

---
+++ JavaScript(基礎編)/メソッドとプロパティー/main.js
@@ -0,0 +1,21 @@
+// let hello = 'hello world11';
+// console.log(hello.length);  //変数に lengthプロパティ を付けることで文字数を取得することができる
+
+// let hello = 'hello world11';
+// console.log(hello.toUpperCase());  //大文字で表示。特定の処理を行うものをメソッド
+
+// let hello = 'hello world11';
+// console.log(hello.substring(0, 5));  //指定した文字数で切り取るメソッド
+
+// let hello = 'hello world11';
+// console.log(hello.slice(0, 5));  //指定した文字数で切り取るメソッド
+
+// let hello = 'hello world11';
+// console.log(hello.split(''));  //文字を分割するメソッド。空文字だと1文字ずつ分割される
+
+let hello = 'hello world11';
+console.log(hello.split(' '));  //スペースを入れると、スペースを境に分割される。これらは配列に格納される
+
+// 末に()がつくものはメソッド。特定の処理を行うためのもの
+
+// ()がつかないものはプロパティ。指示によって特定の値を保持しているもの
