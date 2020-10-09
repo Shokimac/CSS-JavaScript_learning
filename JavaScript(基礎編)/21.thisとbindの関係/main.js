7cc2ab31fcb0ba1408c87380b0fb525c35f96719
Shoki Yokomizo <curin030204@yahoo.co.jp>
2020-10-09 14:02:02 +0900

    thisとbind

--- JavaScript(基礎編)/21.thisとbindの関係/main.js
+++ JavaScript(基礎編)/21.thisとbindの関係/main.js
@@ -9,7 +9,8 @@ const obj = {
             console.log(this);
             // そこで、bindの第一引数にオブジェクトを与えることで参照先を指定することができる。
             // ちなみにbindの引数はオブジェクトであれば良いので、以下のように書くこともできる。
-        }.bind({first_name: 'Mafia'}));
+        }.bind({ first_name: 'Mafia' }));
+        // bindを使うことでthisの参照先を思い通りにできる
     }
 }
 
